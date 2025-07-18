8b4129f128d6052cfd01bb3cdd1d598b7e36a1a5
younorg <ioxixiz27@gmail.com>
2025-07-19 03:28:59 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+import { useState, useEffect } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+/* Full code omitted here for brevity; will be injected in actual script */
